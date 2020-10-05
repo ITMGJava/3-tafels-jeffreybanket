c771d1246edadb695dadd52bfa56d391a071ed6a
jeffreybanket <jeffreybanket@gmail.com>
2020-10-05 21:29:35 +0200

    Ik probeer hier een cijfer invoer te doen en dit te laten loopen, echter dit lukt mij niet ondanks de vele tutorials. ik wil namelijk dat als de gebruiker geen gebruik wilt maken van de calculator dat deze gewoon simpel weg moet typen einde. misschien kan je me hierbij helpen

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,2 +1,28 @@
-public class Tafels {
-}
+import java.util.Scanner;
+
+class Tafel {
+    public static void main(String[] args) {
+
+        class cijfer {
+        }
+        //int cijfer;
+int einde;
+do {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Voer een getal in om de tafel te berekenen, ben je klaar met de calculator typ dan 'Einde':");
+    //cijfer = sc.nextInt();
+    var cijfer = sc.nextLine();
+    for (int x = 1; x <= 10; x++) {
+        System.out.println(cijfer + "*" + x + "=" + (cijfer * x));
+    }
+    if (cijfer.equals(einde)) {
+        System.out.println("U bent gekomen aan het einde van de Calculator");
+    }
+        }
+    }
+        }
+
+
+
+
+
