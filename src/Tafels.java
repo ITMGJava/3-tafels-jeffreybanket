ea4fadeab6ac592ec438fe1ba8f35c4ece61c69a
jeffreybanket <jeffreybanket@gmail.com>
2020-10-05 21:31:39 +0200

    de juiste tafel tot 10 geupdate

--- src/Tafels.java
+++ src/Tafels.java
@@ -5,22 +5,20 @@ class Tafel {
 
         class cijfer {
         }
-        //int cijfer;
-int einde;
-do {
+        int cijfer;
+        int einde;
     Scanner sc = new Scanner(System.in);
     System.out.println("Voer een getal in om de tafel te berekenen, ben je klaar met de calculator typ dan 'Einde':");
-    //cijfer = sc.nextInt();
-    var cijfer = sc.nextLine();
+    cijfer = sc.nextInt();
     for (int x = 1; x <= 10; x++) {
         System.out.println(cijfer + "*" + x + "=" + (cijfer * x));
     }
-    if (cijfer.equals(einde)) {
+   // if (cijfer.equals(einde)) {
         System.out.println("U bent gekomen aan het einde van de Calculator");
-    }
+
         }
     }
-        }
+
 
 
 
